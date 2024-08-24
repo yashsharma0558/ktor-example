dfa4c985d27fead24e33989484e38ae6c8ca639b
Yash <yashsharma0558@gmail.com>
2024-08-24 19:38:24 +0530

    updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,11 @@ application {
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
+ktor {
+    fatJar {
+        archiveFileName.set("fat.jar")
+    }
+}
 
 repositories {
     mavenCentral()
